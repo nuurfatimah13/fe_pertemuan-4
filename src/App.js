852e461ce360fdb3932b57419051a5306a417911
nuurfatimah13 <nuurfatimah13@gmail.com>
2022-03-10 17:28:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
